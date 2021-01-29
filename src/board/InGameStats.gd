f5d75559ef475d8959ec3ceffec2e13207ee06f1
Nick Mertens <nick@thecrypt.co.nz>
2021-01-30 12:35:33 +1300

    Remove money tracker

--- src/board/InGameStats.gd
+++ src/board/InGameStats.gd
@@ -5,22 +5,9 @@ signal out_of_moves
 export (int) var max_moves = 120
 
 onready var remaining_moves: int = max_moves
-var money_earned := 0
-var money_increased_scene = preload("res://src/board/MoneyIncreasedIndicator.tscn")
 
 func _ready():
 	$StatsContainer/Row/MovesRemaining/Label.text = str(remaining_moves)
-
-
-func on_recipe_fulfilled(_type, _ingredients, worth: int):
-	money_earned += worth
-	var money_label = $StatsContainer/Row/Money/Label
-	money_label.text = str(money_earned)
-	
-	var money_increased: Control = money_increased_scene.instance()
-	money_increased.amount = worth
-	money_increased.set_global_position(Vector2($StatsContainer/Row/Money.margin_right, $StatsContainer/Row/Money.margin_top))
-	add_child(money_increased)
 
 
 func valid_move_started():
